2544140efee7bdc9d4472d652e0b02820ab9807e
bwielk <bwielk@gmail.com>
2017-08-06 23:21:14 +0200

    Car class with tests2

--- src/classes/Car.java
+++ src/classes/Car.java
@@ -1,5 +1,62 @@
 package classes;
 
-public class Car {
+public class Car extends Vehicle {
+	
+	private int wheels;
+	private int petrol;
+	private int exhausts;
+	private boolean isManual;
+	private String currentGear;
+	
+	public Car(int engines, int speed, int doors, String name, int model, int id, int wheels, int petrol, int exhausts,
+			boolean isManual, String currentGear) {
+		super(engines, speed, doors, name, model, id);
+		this.wheels = wheels;
+		this.petrol = petrol;
+		this.exhausts = exhausts;
+		this.isManual = isManual;
+		this.currentGear = currentGear;
+	}
 
-}
+	public int getWheels() {
+		return wheels;
+	}
+
+	public void setWheels(int wheels) {
+		this.wheels = wheels;
+	}
+
+	public int getPetrol() {
+		return petrol;
+	}
+
+	public void setPetrol(int petrol) {
+		this.petrol = petrol;
+	}
+
+	public int getExhausts() {
+		return exhausts;
+	}
+
+	public void setExhausts(int exhausts) {
+		this.exhausts = exhausts;
+	}
+
+	public boolean isManual() {
+		return isManual;
+	}
+
+	public void setManual(boolean isManual) {
+		this.isManual = isManual;
+	}
+
+	public String getCurrentGear() {
+		return currentGear;
+	}
+
+	public void setCurrentGear(String currentGear) {
+		this.currentGear = currentGear;
+	}
+	
+	};
+	
--- src/classes/CarTest.java
+++ src/classes/CarTest.java
@@ -2,13 +2,39 @@ package classes;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CarTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	private Car car1;
+	
+	@Before
+	public void before(){
+		car1 = new Car(1, 80, 4, "Seat", 4556, 123, 4, 200, 5, true, "Extra seats");
 	}
-
+	
+	@Test
+	public void canGetNumberOfEngines(){
+		int numOfEngines = car1.getEngine();
+		assertEquals(1, numOfEngines);
+	}
+	
+	@Test 
+	public void canGetSpeed(){
+		int speedValue = car1.getSpeed();
+		assertEquals(80, speedValue);
+	}
+	
+	@Test 
+	public void canRide(){
+		String result = car1.ride();
+		assertEquals("The vehicle number 123 moves on", result);
+	}
+	
+	@Test
+	public void canAccelarate(){
+		car1.accelarate(1000);
+		assertEquals(1080, car1.getSpeed());
+	}
 }
