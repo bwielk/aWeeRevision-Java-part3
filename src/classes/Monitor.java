0481da88db2335ceb3e4696daf0a5ef02e5b22b7
bwielk <bwielk@gmail.com>
2017-08-07 19:21:11 +0200

    Composition exercise

---
+++ src/classes/Monitor.java
@@ -0,0 +1,50 @@
+package classes;
+
+public class Monitor {
+	
+	private String model;
+	private String manufacturer;
+	private int size;
+	private Resolution resolution;
+	
+	public Monitor(String model, String manufacturer, int size, Resolution resolution) {
+		super();
+		this.model = model;
+		this.manufacturer = manufacturer;
+		this.size = size;
+		this.resolution = resolution;
+	}
+	
+	public void drawPixelAt(int x, int y, String colour){
+		System.out.println("Drawing pixel at " + x + "," + y + " in colour " + colour);
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public Resolution getResolution() {
+		return resolution;
+	}
+	
+}
